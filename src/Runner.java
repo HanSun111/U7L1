1ff2e4ca2217619c44cd033b397a3a5c3526b3b2
BT_4N2_13 <hsun7920@bths.edu>
2023-02-01 12:19:19 -0500

    finished

--- src/Runner.java
+++ src/Runner.java
@@ -3,26 +3,14 @@ import java.util.ArrayList;
 public class Runner {
     public static void main(String[] args) {
 
-        ArrayList<Cat> cats = new ArrayList<Cat>();
-        Cat c1 = new Cat();
-        c1.setCatName("Tails");
-        cats.add(c1);
-        Cat c2 = new Cat();
-        c2.setCatName("Tail");
-        cats.add(c2);
-        Cat c3 = new Cat();
-        c3.setCatName("Tai");
-        cats.add(c3);
-        Cat c4 = new Cat();
-        c4.setCatName("Ta");
-        cats.add(c4);
+        ArrayList<Dog> dogList = new ArrayList<Dog>();
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog();
+        dog1.setDogName("Spot");
+        dog2.setDogName("Benny");
+        dogList.add(dog1);
+        dogList.add(dog2);
+        dogList.get(0).bark();
 
-        Cat newCat = new Cat();
-        newCat.setCatName("Ginger");
-        cats.set(2, newCat);
-
-        cats
-
-        System.out.println(cats);
     }
 }
